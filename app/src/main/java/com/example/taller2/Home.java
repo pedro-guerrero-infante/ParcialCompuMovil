a0ce44fa32ddbf8f399d1b21844600b459e8c8fd
proxar <pedroguerrero902@gmail.com>
2021-05-28 00:50:54 -0500

    Actividad 1,2,3 completas

--- app/src/main/java/com/example/taller2/Home.java
+++ app/src/main/java/com/example/taller2/Home.java
@@ -5,19 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
-
-import com.example.taller2.Auxiliares.UsuarioAux;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -32,7 +27,6 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
-    private String PATH_LOCATION = "locations";
     private GoogleMap mMap;
     private String email;
 
@@ -40,8 +34,8 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        mAuth = FirebaseAuth.getInstance();
 
+        mAuth = FirebaseAuth.getInstance();
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
@@ -55,13 +49,15 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
         int itemClicked = item.getItemId();
-        if(itemClicked == R.id.menuLogOut){
+        if(itemClicked == R.id.menuLogOut)
+        {
             mAuth.signOut();
             Intent intent = new Intent(this, MainActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
+
         }else if (itemClicked == R.id.menuSettings){
-//Abrir actividad para configuración etc
+            //Abrir actividad para configuración etc
         }
         return super.onOptionsItemSelected(item);
     }
@@ -69,74 +65,66 @@ public class Home extends AppCompatActivity implements OnMapReadyCallback {
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        final double[] lat = {0};
-        final double[] lng = {0};
-        System. out. println("Voy aqui");
         database = FirebaseDatabase.getInstance();
         FirebaseUser usuario = mAuth.getCurrentUser();
         String id = usuario.getUid();
-        System. out. println("Usuario" + id);
+
         myRef = FirebaseDatabase.getInstance().getReference().child("Usuarios").child(id);
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    lat[0] = Double.parseDouble(dataSnapshot.child("latitud").getValue().toString());
-                    lng[0] = Double.parseDouble(dataSnapshot.child("longitud").getValue().toString());
-                    System. out. println("Ahora aqui");
-                    System. out. println("Latitud-------" + dataSnapshot.child("latitud").getValue().toString());
-                    System. out. println("Longitud-------" + dataSnapshot.child("longitud").getValue().toString());
-
-                    LatLng ubi = new LatLng(Double.parseDouble(dataSnapshot.child("latitud").getValue().toString()), Double.parseDouble(dataSnapshot.child("longitud").getValue().toString()));
-                    mMap.addMarker(new MarkerOptions().position(ubi).title("Marker en Sidney"));
+                if (dataSnapshot.exists())
+                {
+                    LatLng ubi = new LatLng(Double.parseDouble(dataSnapshot.child("latitud").getValue().toString()),
+                            Double.parseDouble(dataSnapshot.child("longitud").getValue().toString()));
+                    mMap.addMarker(new MarkerOptions().position(ubi).title("Tu ubicación"));
                     mMap.moveCamera(CameraUpdateFactory.newLatLng(ubi));
 
                     mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
                     mMap.getUiSettings().setZoomGesturesEnabled(true);
                     mMap.getUiSettings().setZoomControlsEnabled(true);
-
-                    Marker mfBogota = mMap.addMarker(new MarkerOptions().position(ubi).
-                            title("Tu ubicacion")
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
-                }
-                System. out. println("Y aqui");
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        System. out. println("Latitud" + lat);
-        System. out. println("Longitud" + lng);
-
-    }
-
-    public void ubicaciones(){
-        final double[] lat = {0};
-        final double[] lng = {0};
-
-        myRef = FirebaseDatabase.getInstance().getReference().child("locations");
-        myRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                if (dataSnapshot.exists()) {
-                    lat[0] = Double.parseDouble(dataSnapshot.child("latitude").getValue().toString());
-                    lng[0] = Double.parseDouble(dataSnapshot.child("longitude").getValue().toString());
                 }
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
             }
         });
+
+        int n = 0;
+        while(n < 5) {
+
+            myRef = FirebaseDatabase.getInstance().getReference().child("locations").child(n+"");
+            myRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot)
+                {
+                    if (dataSnapshot.exists())
+                    {
+                        System.out.println(dataSnapshot.child("latitude").getValue().toString());
+                        System.out.println(dataSnapshot.child("longitude").getValue().toString());
+                        System.out.println(dataSnapshot.child("name").getValue().toString());
+
+
+                        LatLng ubi = new LatLng(Double.parseDouble(dataSnapshot.child("latitude").getValue().toString()),
+                                                Double.parseDouble(dataSnapshot.child("longitude").getValue().toString()));
+
+                        mMap.addMarker(new MarkerOptions().position(ubi).title(dataSnapshot.child("name")
+                                .getValue().toString()).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(ubi));
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
+            n++;
+        }
     }
 
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
